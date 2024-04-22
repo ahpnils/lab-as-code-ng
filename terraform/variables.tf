652ae047f713a25dfb89f5e64b6cb2dd13e711f2
Nils Ratusznik <nils.github@anotherhomepage.org>
2024-04-22 07:54:49 +0200

    Add Debian nodes for Kubernetes tests.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,6 +14,15 @@ variable "fedora_image_source" {
   default = "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/x86_64/images/Fedora-Cloud-Base-Generic.x86_64-40-1.14.qcow2"
 }
 
+variable "debian_image_source" {
+  # https://mop.koeln/blog/creating-a-local-debian-vm-using-cloud-init-and-libvirt/
+  # Obviously this sounds like the right image. Unfortunately it is not. It doesn't contain 
+  # the SATA AHCI drivers which are needed because of the way the cloud-init stuff is being 
+  # injected into the VM (as a cdrom drive).
+  # default = "https://cloud.debian.org/images/cloud/bookworm/daily/latest/debian-12-genericcloud-amd64-daily.qcow2"
+  default = "https://cloud.debian.org/images/cloud/bookworm/daily/latest/debian-12-generic-amd64-daily.qcow2"
+}
+
 variable "libvirt_uri" {
   default = "qemu:///system"
 }
@@ -28,4 +37,20 @@ variable "nodes_quantity" {
 
 variable "nodes_offset" {
   default = 10
+}
+
+variable "control_quantity" {
+  default = 1
+}
+
+variable "control_offset" {
+  default = 20
+}
+
+variable "worker_quantity" {
+  default = 1
+}
+
+variable "worker_offset" {
+  default = 30
 }
